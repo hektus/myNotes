d78120d10b3b5901d7f32ec10d8aceed08f054b2
Dawid Dzierzega <70227471+hektus@users.noreply.github.com>
2024-02-04 12:58:10 +0100

    added colors for various notes

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const addBtn = document.querySelector(".add");
 const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
 const popupAdd = document.querySelector(".popup-add");
 const allNotes = document.querySelector(".notes");
 const error = document.querySelector(".error");
 
 const categorySelect = document.querySelector("#select");
 const noteInput = document.querySelector(".note-input");
+
+function titleColor() {
+  if (categorySelect.selectedIndex === 1) {
+    return "title title-red";
+  } else if (categorySelect.selectedIndex === 2) {
+    return "title";
+  } else {
+    return "title title-blue";
+  }
+}
+
+function contentColor() {
+  if (categorySelect.selectedIndex === 1) {
+    return "note-content note-red";
+  } else if (categorySelect.selectedIndex === 2) {
+    return "note-content";
+  } else {
+    return "note-content note-blue";
+  }
+}
 
 function saveNote() {
   const selectedOption = categorySelect.options[categorySelect.selectedIndex];
@@ -17,12 +38,13 @@ function saveNote() {
   const newNote = document.createElement("div");
   newNote.classList.add("note");
   allNotes.appendChild(newNote);
+
   newNote.innerHTML = `
-    <div class="title">
+    <div class="${titleColor()}">
       <p class="note-title">${selectedOption.textContent}</p>
       <p class="delete-note">X</p>
     </div>
-    <p class="note-content">${noteInput.value}</p>
+    <p class="${contentColor()}">${noteInput.value}</p>
   `;
 }
 
@@ -32,3 +54,6 @@ addBtn.addEventListener("click", () => {
 });
 
 saveBtn.addEventListener("click", saveNote);
+cancelBtn.addEventListener("click", () => {
+  popupAdd.style.display = "none";
+});
